f2e25f2d037af6e55d9fd40cc3bab5ed5435c283
nayeong2 <hello6599@naver.com>
2018-08-01 16:26:56 +0900

    create fullcalenderAPI config file

--- src/main/java/com/groupware/schedule/service/ScheduleServiceImpl.java
+++ src/main/java/com/groupware/schedule/service/ScheduleServiceImpl.java
@@ -5,18 +5,19 @@ import java.util.List;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.groupware.schedule.model.ScheduleDivisionDto;
 
 @Service
 public class ScheduleServiceImpl implements ScheduleService {
 
-	
-	
+
 	@Autowired
 	private SqlSession sqlSession;
 		
 	@Override
+	@Transactional
 	public List<ScheduleDivisionDto> getListSchedule() {
 		// TODO Auto-generated method stub
 		return null;
